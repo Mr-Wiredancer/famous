9d52c63d63c7736dcf63abc79f88c8fb9395c0a6
Mr-Wiredancer <lijiahao90@gmail.com>
2014-08-05 15:40:26 +0800

    annotate ElementAllocator

--- core/ElementAllocator.js
+++ core/ElementAllocator.js
@@ -1,3 +1,4 @@
+//中文注释部分由 Mr. Wiredancer(aka J.L) 完成
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -9,6 +10,7 @@
 
 define(function(require, exports, module) {
 
+
     /**
      * Internal helper object to Context that handles the process of
      *   creating and allocating DOM elements within a managed div.
@@ -19,10 +21,14 @@ define(function(require, exports, module) {
      * @private
      * @param {Node} container document element in which Famo.us content will be inserted
      */
+    //`ElementAllocator`应该作为一个私有类来使用。它是专门用来生成一个给`Context`内部使用的对象，这个对象是负责`Context`所管理的 div(`container`) 下面的 DOM 元素的创建和分配
     function ElementAllocator(container) {
+        //注意此处使用了`DocumentFragment`，这个对象会带来非常高的性能提升。简单来说，`DocumentFragment`只储存在内存而不在 DOM 树中，所以将元素插入`DocumentFragment`时不会引起 reflow. 详情参见 https://developer.mozilla.org/en-US/docs/Web/API/document.createDocumentFragment
         if (!container) container = document.createDocumentFragment();
         this.container = container;
+        //`detachedNodes`是一个存放可重用的 DOM 元素的池
         this.detachedNodes = {};
+        //`nodeCount`记录这个对象所管理的通过`allocate`分配的非空闲元素的个数
         this.nodeCount = 0;
     }
 
@@ -34,10 +40,12 @@ define(function(require, exports, module) {
      *
      * @param {Node} container document element to which Famo.us content will be migrated
      */
+    // 将原有container的 DOM 元素转移到新的container去，并管理新的 container 
     ElementAllocator.prototype.migrate = function migrate(container) {
         var oldContainer = this.container;
         if (container === oldContainer) return;
 
+        //如果`container`是一个`DocumentFragment`对象, 则直接将`this.container`加到`container`下面
         if (oldContainer instanceof DocumentFragment) {
             container.appendChild(oldContainer);
         }
@@ -59,6 +67,7 @@ define(function(require, exports, module) {
      * @param {string} type type of element, e.g. 'div'
      * @return {Node} allocated document element
      */
+    // 如果空闲池里有所需类型的空闲元素，则返回一个空闲元素；否则创建一个新元素并返回
     ElementAllocator.prototype.allocate = function allocate(type) {
         type = type.toLowerCase();
         if (!(type in this.detachedNodes)) this.detachedNodes[type] = [];
@@ -83,6 +92,7 @@ define(function(require, exports, module) {
      *
      * @param {Node} element document element to deallocate
      */
+    // 将一个可以重用的元素放到空闲池去
     ElementAllocator.prototype.deallocate = function deallocate(element) {
         var nodeType = element.nodeName.toLowerCase();
         var nodeStore = this.detachedNodes[nodeType];
@@ -98,6 +108,7 @@ define(function(require, exports, module) {
      *
      * @return {Number} total node count
      */
+    // getter of this.nodeCount
     ElementAllocator.prototype.getNodeCount = function getNodeCount() {
         return this.nodeCount;
     };
